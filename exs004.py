339204c7b52776a6af97003e121d01a49e87ffb1
Diego Figueiredo <diego.silva@sou.inteli.edu.br>
2025-09-08 19:32:56 -0300

    feat: new

--- exs004.py
+++ exs004.py
@@ -2,6 +2,4 @@ word=input('Digite alguma coisa: ')
 print('O tipo primitivo é ', type(word))
 print('tem somente espaços? ', word.isspace())
 print('É um numero ', word.isnumeric())
-
-
-
+print('Está em maiusculo? ', word.isupper())
