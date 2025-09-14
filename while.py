ee7e53be4d7a59b8e5d0c127906ec130171e7d6d
Diego Figueiredo <diego.silva@sou.inteli.edu.br>
2025-09-14 09:55:09 -0300

    feat: while com if

--- while.py
+++ while.py
@@ -3,3 +3,11 @@ cont=0
 while cont <= 8:
     print(f"o contandor está na posição {cont}ª")
     cont+=1
+
+
+while True:
+    dig=int(input("digite um num par: "))
+    if dig%2 == 0:
+        print("vc digitou um num par, thx")
+    else:
+        print("você digitou um num impar, seu burro")
